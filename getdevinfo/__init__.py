dff264fcec17808c6e09c67af8577bc0de818cd9
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-01-12 11:46:46 +0000

    Update getdevinfo/__init__.py so the wheels work

--- getdevinfo/__init__.py
+++ getdevinfo/__init__.py
@@ -15,3 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with GetDevInfo.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import
+from . import getdevinfo
