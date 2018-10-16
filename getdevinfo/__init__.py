f60f0958874dfa4302964ecc611c4519080c0f5a
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-10-16 11:42:29 +0100

    Bump version to 1.0.4

--- getdevinfo/__init__.py
+++ getdevinfo/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Device Information Obtainer Package 1.0.3
+# Device Information Obtainer Package 1.0.4
 # This file is part of GetDevInfo.
 # Copyright (C) 2013-2018 Hamish McIntyre-Bhatty
 # GetDevInfo is free software: you can redistribute it and/or modify it
