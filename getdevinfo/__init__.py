59c45e660a6f2eb497d5660dc36116f5707ac227
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-02-19 15:19:57 +0000

    Revert annoying RuntimeWarning fix because it breaks backwards-compatibility in a strange way

--- getdevinfo/__init__.py
+++ getdevinfo/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GetDevInfo.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import
+from . import getdevinfo
 
 if __name__ == "__main__":
-    from . import getdevinfo
     getdevinfo.run()
